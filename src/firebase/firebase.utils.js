43756343eacf6bc4f6d5970b89011cdbc0c8139a
tguney <tguney@hotmail.com>
2022-01-20 18:23:13 +0300

    created cart-item component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,12 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const config = {
-  apiKey: "AIzaSyChDrIspROjj_Ny014xCk_X1NUZOl4YrF8",
-  authDomain: "crwn-db-3e017.firebaseapp.com",
-  projectId: "crwn-db-3e017",
-  storageBucket: "crwn-db-3e017.appspot.com",
-  messagingSenderId: "102551215477",
-  appId: "1:102551215477:web:5abe5ab175707f506d873c"
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
